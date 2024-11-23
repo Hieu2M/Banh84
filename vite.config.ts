e951681b90cb306477c533d8a9539aa20cf4cbf7
Hieu2M <thuonghieutk1996@gmail.com>
2024-11-22 23:47:46 -0800

    fix the dist 3rd time

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Banh84/",
+  base: './', // Relative paths
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
