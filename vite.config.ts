8c92c19d7af261f3534e02ea622e95c0c0f552b5
Hieu2M <thuonghieutk1996@gmail.com>
2024-11-23 10:41:17 -0800

    5th fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Banh84/", // Relative paths
+  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
